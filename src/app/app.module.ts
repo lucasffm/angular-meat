5733d3612a52161cc63bc86ce8f31120d09a2181
Lucas de Freitas <lucasffm@pop-os.localdomain>
2020-02-04 16:18:00 -0300

    restaurant detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { RestaurantsComponent } from "./restaurants/restaurants.component";
 import { RestaurantComponent } from "./restaurants/restaurant/restaurant.component";
 import { RestaurantsService } from "./restaurants/restaurants.service";
 import { HttpClientModule } from "@angular/common/http";
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +23,11 @@ import { HttpClientModule } from "@angular/common/http";
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent
   ],
   imports: [BrowserModule, RouterModule.forRoot(ROUTES), HttpClientModule],
   providers: [RestaurantsService],
