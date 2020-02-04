237887ec12e38c255509fed1d59766587b53cd57
Lucas de Freitas <lucasffm@pop-os.localdomain>
2020-02-04 15:05:01 -0300

    list restaurants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { ROUTES } from "./app.routes";
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent, RestaurantsComponent, RestaurantComponent],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent]
 })
